0d07204dd41a6cafce19d09448522c943de7e20a
Jane <apiwatsupaphol@gmail.com>
2024-12-15 20:34:08 +0700

    feat: added profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,59 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="max-h-screen overflow-hidden h-screen w-full flex items-center justify-center p-4 relative bg-primary-darkCyan">
+      <section className="absolute -right-32 bottom-0 transform  w-1/2 bg-pattern-top h-1/2"></section>
+      <section className="absolute -left-0 -top-48 transform rotate-90  w-1/2 bg-pattern-bottom h-1/2 bg-cover bg-no-repeat"></section>
+      <section className="w-96 flex flex-col ">
+        <div>
+          <Image
+            src={"/images/bg-pattern-card.svg"}
+            alt="background pattern"
+            width={384}
+            height={384}
+            className="rounded-t-2xl"
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex flex-col items-center justify-center gap-2 bg-white rounded-b-2xl p-6">
+          <div>
+            <Image
+              src={"/images/image-victor.jpg"}
+              alt="Victor Crest"
+              width={96}
+              height={96}
+              className="rounded-full border-4 border-white -mt-16"
+            />
+          </div>
+
+          <div className="flex flex-col items-center gap-1">
+            <h1 className="font-bold text-primary-veryDarkDesaturatedBlue">
+              Victor Crest <span className="text-second-darkGray">26</span>
+            </h1>
+            <h1 className="text-sm text-second-darkGray">London</h1>
+          </div>
+          <hr className="w-full border-gray-200 border-1 my-3" />
+          <div className="flex items-center justify-between w-3/4 mx-auto">
+            <div className="flex flex-col items-center">
+              <h1 className="font-bold text-primary-veryDarkDesaturatedBlue">
+                80K
+              </h1>
+              <h1 className="text-xs text-second-darkGray ">Followers</h1>
+            </div>
+            <div className="flex flex-col items-center">
+              <h1 className="font-bold text-primary-veryDarkDesaturatedBlue">
+                803K
+              </h1>
+              <h1 className="text-xs text-second-darkGray ">Likes</h1>
+            </div>
+            <div className="flex flex-col items-center">
+              <h1 className="font-bold text-primary-veryDarkDesaturatedBlue">
+                1.4K
+              </h1>
+              <h1 className="text-xs text-second-darkGray ">Photos</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,19 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          darkCyan: "hsl(185, 75%, 39%)",
+          veryDarkDesaturatedBlue: "hsl(229, 23%, 23%)",
+          darkGrayishBlue: "hsl(227, 10%, 46%)",
+        },
+        second: {
+          darkGray: "hsl(0, 0%, 59%)",
+        },
+      },
+      backgroundImage: {
+        card: "url('/images/bg-pattern-card.svg')",
+        "pattern-top": "url('/images/bg-pattern-top.svg')",
+        "pattern-bottom": "url('/images/bg-pattern-bottom.svg')",
       },
     },
   },
